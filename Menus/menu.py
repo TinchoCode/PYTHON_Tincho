9b1d4fadbc54b5c126f2d5807e28a0eb70a05564
TinchoCode <89424821+TinchoCode@users.noreply.github.com>
2023-03-05 17:51:56 -0300

    Update menu.py

--- Menus/menu.py
+++ Menus/menu.py
@@ -7,23 +7,23 @@ mFrame= tk.Frame(window) #Main Frame
 mFrame.pack(fill= "both", expand= True, padx= 5, pady= 5)
 mFrame.config(bg= "black")
 
-menuF= tk.Frame(mFrame, bg= "red") #Frame para menu
+menuF= tk.Frame(mFrame, bg= "red") #Frame para el menu
 menuF.pack(fill= "x")
 
-for i in range(5):
-#Cuadros del menu
-    col= tk.Frame(menuF, borderwidth= 1, relief= "solid", width= 50, height= 20, bg= "yellow")
+for i in range(5): #Loop para crear cada item
+#Fila de Frames
+    col= tk.Frame(menuF, borderwidth= 1, relief= "solid", width= 50, height= 20, bg= "blue")
     col.grid(row=0, column= i, padx= 1, pady= 1)
-#InstaBotones del menu
-    si= str(i + 1)
+#Fila de Botones (Sin uso)
+    si= str(i + 1) #Agrego esta variable string para poder concatenarla en el text
     button= tk.Button(menuF, borderwidth= 1, relief= "solid", bg= "yellow", text= "Soy "+si)
     button.grid(row=1, column= i, padx= 1, pady= 1)
 
-cuadroF= tk.Frame(mFrame, bg= "lightblue", height= 100)
+cuadroF= tk.Frame(mFrame, bg= "lightblue", height= 100) #Aca establezco el height
 cuadroF.pack(fill="both", expand= True)
-cuadroF.propagate(False)
+cuadroF.propagate(False) #Con esto evito que tome las medidas del "Padre" y use la establecida por mi
 
-veo= tk.Label(cuadroF, text="Soy el mundo")
+veo= tk.Label(cuadroF, text="Soy el mundo") #Label agregado para que el segundo Frame no este vacio
 veo.pack()
 
 window.mainloop()
